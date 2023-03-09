100ed9a17855026b7f322fd00761b113b815b0bf
Utelagazkodas <127396354+Utelagazkodas@users.noreply.github.com>
2023-03-09 19:35:37 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
+// MODULES
 const http = require("http")
 const fs = require("fs")
 const express = require("express")
 const bodyParser = require("body-parser")
 const WebSocket = require("ws")
 const { json } = require("body-parser")
+const { Console } = require("console")
 
 // PORT
 const port = 80
 
+// non module variables
 const app = express()
-
 const wss = new WebSocket.Server({ port: 443 });
 
+// sets up json on express server
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+// the files the clients can access
 const accessableFiles =[
     "/index.html",
     "/images/juan.jpg",
@@ -22,9 +26,11 @@ const accessableFiles =[
     "/images/vatican.jpg",
     "/page.html",
     "/secret.html",
-    "/chat.html"
+    "/chat.html",
+    "/favicon.ico"
 ]
 
+// the urls that redirect and what they redirect to
 const redirects= {
     "/" : "/index.html",
     "/favcolor" : "/page.html",
@@ -33,25 +39,41 @@ const redirects= {
 
 var messages = []
 
+// called when an error accoures prints out a comment and general info to the console
+function error(req, res, comment){
+    console.log("\n" + comment)
+    console.log("error accoured when " + req.ip + " tried accesing " + req.path)
+    res.write("error 404")
+}
+
+// runs when the server gets a get request to any URL
 app.get("*", (req, res) =>{
+
+    console.log("\n" + "GET request made to: "+ req.path)
+    console.log("from ip: " + req.ip + "\n")
 
     // if the file ends in html then sets the header to html
     if(req.path.includes(".html")){
         res.writeHead(200, {"Content-Type" : "text/html"})
     }
 
+    // if the url is a redirect, redirects the client
     if(redirects[req.path] != undefined){
         res.write("<Script>window.location.replace(window.location.origin +'" + redirects[req.path] +"')</Script>")
         res.end()
         return
     }
+
     else{
 
+        //checks if the file the client is trying to acces accessable
         if(accessableFiles.includes(req.path)){
+
+            // reads the file the client is trying to access
             fs.readFile(req.path.substring(1), (err, data) => {
     
                 if(err || data === "undefined"){
-                    res.write("404 something went wrong 404")
+                    error(req, res, "An error accured while trying to read file")
                     res.end()
                     return
                 }else{        
@@ -61,15 +83,20 @@ app.get("*", (req, res) =>{
                 }
             })
         }
+        // sends error if the file is unacsacceble
         else{
-            res.write("404 something went wrong 404")
+            error(req, res, "File the client is trying to acces is not accesable or doesnt exist")
             res.end()
             return
         }
     }
 })
 
+// runs when the server receives a post request to /favcolor
 app.post("/favcolor", (req, res) =>{
+
+    console.log("\n" + "POST request made to /favcolor")
+    console.log("from ip: "+ req.ip  + "\n")
 
     res.writeHead(200, {"Content-Type" : "text/html"})
   
@@ -78,14 +105,20 @@ app.post("/favcolor", (req, res) =>{
     res.end()
 })
 
-
+// websocket of the chat server
 wss.on("connection", function connection(ws){
+    console.log("\n" + "someone connected to the chat" + "\n")
+
+    // sends the messages every time someone connects to the websocket
     ws.send(JSON.stringify(messages))
 
+    // runs when server recieves a message to websocket
     ws.on('message', function message(data) {
+        console.log("\n" + "new message made to chat: " + data.toString())
 
         messages.push(data.toString())
 
+        // sends updated messages to every websocket
         wss.clients.forEach(function each(client){
             if(client.readyState === WebSocket.OPEN){
                 client.send(JSON.stringify(messages))
@@ -95,5 +128,5 @@ wss.on("connection", function connection(ws){
     });
 })
 
-
+// listens on port
 app.listen(port)
